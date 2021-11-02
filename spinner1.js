d995e3348a6b9c7fb0ca1094cb61cc1e538758cb
Victor Bondarenko <nirasimha@gmail.com>
2021-11-02 18:25:20 +0000

    Spinner 2 added with for..loop

--- spinner1.js
+++ spinner1.js
@@ -1,11 +1,32 @@
-const sentence = "\r|\r/\r-\r\\\r|\r/\r-\r\\\r|\r/\r-\r\\\n";
-const text = sentence.slice("");
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
 
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
 
-let delay = 100;
-for (const char of text) {
-  setTimeout(() => {
-    process.stdout.write(char);
-  },delay);
-  delay += 100;
-}
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+
+  process.stdout.write('\r\\   ');
+}, 1500);
